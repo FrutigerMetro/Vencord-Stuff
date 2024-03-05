23d5cb41963f238bf31612d48cd6f675f47c66b4
Tony <73192529+twixhood@users.noreply.github.com>
2024-03-04 18:26:08 -0600

    changes lol

---
+++ Plugins/skypeloginsound.ts
@@ -0,0 +1,18 @@
+import definePlugin from "../utils/types";
+import { Devs } from "../utils/constants";
+
+
+export default definePlugin({
+    name: "SkypeStartupSound",
+    description: "replace the \"discordo\" sound on startup with the skype login sound",
+    authors: [
+        Devs.echo,
+    ],
+    patches: [{
+        find: "44208515805198d4c548.mp3",
+        replacement: {
+            match: /e\.exports=n\.p\+\"[a-zA-Z0-9]+\.mp3\"/,
+            replace: 'e.exports="https://raw.githubusercontent.com/twixhood/Vencord-Stuff/raw/main/SkypeLogIn.mp3"'
+        }
+    }]
+});
